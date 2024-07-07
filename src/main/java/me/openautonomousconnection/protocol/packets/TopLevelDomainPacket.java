4b9a1d7dbaeb0f1def16273ae08e324f4f256181
Finn <finn.abadi@outlook.de>
2024-07-07 18:06:35 +0200

    Added Copyright nodes

--- src/main/java/me/openautonomousconnection/protocol/packets/TopLevelDomainPacket.java
+++ src/main/java/me/openautonomousconnection/protocol/packets/TopLevelDomainPacket.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2024 Open Autonomous Connection - All Rights Reserved
+ *
+ * You are unauthorized to remove this copyright.
+ * You have to give Credits to the Author in your project and link this GitHub site: https://github.com/Open-Autonomous-Connection
+ * See LICENSE-File if exists
+ */
+
 package me.openautonomousconnection.protocol.packets;
 
 import me.finn.libraries.eventsystem.EventManager;
