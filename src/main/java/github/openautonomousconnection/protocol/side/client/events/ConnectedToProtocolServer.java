53ea9aff0fb73142067f32ca1552dd95d23e7e50
Finn <83773882+UnlegitDqrk@users.noreply.github.com>
2025-09-24 20:33:24 +0200

    Delete src/main/java/github/openautonomousconnection/protocol directory

--- src/main/java/github/openautonomousconnection/protocol/side/client/events/ConnectedToProtocolServer.java
+++
@@ -1,6 +0,0 @@
-package github.openautonomousconnection.protocol.side.client.events;
-
-import me.finn.unlegitlibrary.event.impl.Event;
-
-public class ConnectedToProtocolServer extends Event {
-}
--- src/main/java/github/openautonomousconnection/protocol/versions/v1_0_0/classic/Classic_LocalDomain.java
+++
@@ -1,7 +0,0 @@
-package github.openautonomousconnection.protocol.versions.v1_0_0.classic;
-
-public class Classic_LocalDomain extends Classic_Domain {
-    public Classic_LocalDomain(String name, String endName, String path) {
-        super(name, endName, null, path);
-    }
-}
